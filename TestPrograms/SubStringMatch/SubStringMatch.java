684dbe16fbf8cc541670f234a6f768c5feeb3c22
Manoj Yerra <manojyerra@gmail.com>
2017-06-17 07:01:51 +0530

    Added comments.

--- TestPrograms/SubStringMatch/SubStringMatch.java
+++ TestPrograms/SubStringMatch/SubStringMatch.java
@@ -8,6 +8,7 @@ class SubStringMatch
 	}
 	
 	
+	
 	SubStringMatch() throws Exception
 	{
 		String str1 = "Hi this is manoj, working in konylabs and working thet company";
@@ -69,3 +70,23 @@ class SubStringMatch
 		return charsMatchCount;
 	}
 }
+
+/*
+1. Containers in c++
+2. Design patterns in c++
+3. Copy constructor in c++  http://www.geeksforgeeks.org/copy-constructor-in-cpp/
+4. deep copy shallow copy in c++
+5. What is class, object, encapsulation, inheritance, Runtime polymorphism.
+6. Virtual keywords
+7. Private virtual functions behavior in run time polymorphism
+8. virtual destructors
+9. dynamic casting and static casting and interprete casting
+10. Data Structures
+11. Multi Threading, Semaphores and Metaphores
+12. Map, Vectors Usage and Advantages and dis-advantages
+13. Sorted Map and UnSorted Map usage
+14. Mutable array and unMutable array
+15. Volatile and extern Keywords Usage
+16. Smart pointers
+17. Deletion of double dimension array
+*/
