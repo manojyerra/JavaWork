7c07b91eb8c86f65aa45df71a2daa50302e8bc52
ManojYerra <manojyerra@gmail.com>
2018-07-23 12:17:23 -0700

    Added postgres jar and modified JDBCTest.java

--- JDBCWork/JDBCTest.java
+++ JDBCWork/JDBCTest.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement; 
 
 class JDBCTest
 {
@@ -15,18 +15,35 @@ class JDBCTest
 	{
 		Connection conn=null;
 
-		try 
+		try
 		{
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+			//Driver and JDBC URL for postgres db
+			Class.forName("org.postgresql.Driver").newInstance();
+			conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
 
-			conn = DriverManager.getConnection("jdbc:sqlserver://10.224.212.18:1433;databaseName=head6024", "prpcuser", "prpcuser");
+			//Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+			//conn = DriverManager.getConnection("jdbc:sqlserver://10.224.212.18:1433;databaseName=head6024", "prpcuser", "prpcuser");
 
 			if(conn!=null)
 			{
 				System.out.println("Database Successfully connected");
-				conn.close();
 			}
 
+			Statement stmt = conn.createStatement();
+
+      			//stmt.execute("create table Employeee123(id integer)");
+			
+			stmt.execute("INSERT INTO Employeee123 VALUES (123)");
+
+			if(stmt != null)
+			{
+				stmt.close();
+			}
+
+			if(conn != null)
+			{
+				conn.close();
+			}
 		} 
 		catch (SQLException e) 
 		{
@@ -34,3 +51,6 @@ class JDBCTest
 		}		
 	}
 }
+
+
+
