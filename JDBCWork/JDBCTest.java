d0a444e05c552419e753c904a34e178de50f10e1
ManojYerra <manojyerra@gmail.com>
2018-07-28 13:16:33 -0700

    Added DBUtils.java

--- JDBCWork/JDBCTest.java
+++ JDBCWork/JDBCTest.java
@@ -3,6 +3,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement; 
+import java.sql.DatabaseMetaData;
+import java.util.ArrayList;
 
 class JDBCTest
 {
@@ -19,27 +21,48 @@ class JDBCTest
 		{
 			//Driver and JDBC URL for postgres db
 			Class.forName("org.postgresql.Driver").newInstance();
-			conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/postgres", "postgres", "postgres");
+			conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
 
 			//Driver and JDBC URL for mssql db
 			//Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
-			//conn = DriverManager.getConnection("jdbc:sqlserver://10.224.212.18:1433;databaseName=head6024", "prpcuser", "prpcuser");
+			//conn = DriverManager.getConnection("jdbc:sqlserver://192.168.43.130:1433;databaseName=head6024", "prpcuser", "prpcuser");
 
 			if(conn!=null)
 			{
 				System.out.println("Database Successfully connected");
 			}
 
-			Statement stmt = conn.createStatement();
-			ResultSet rs = st.executeQuery(query);
+			
+			long startTime = System.currentTimeMillis();
+
+			ArrayList<String> tablesList = DBUtils.getTablesList(conn, false);
+
+
+			for(int i=0; i<tablesList.size(); i++)
+			{
+				String tableName = tablesList.get(i);
+				System.out.println("tableName : "+tableName);			
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery("select * from "+tableName);
+				DBUtils.printTable(rs);
+				stmt.close();
+			}
+
+
+			long timeTaken = System.currentTimeMillis() - startTime;
+
+			System.out.println("Time taken : "+timeTaken+", number of tables : "+tablesList.size());
+
+			
+
 
       			//stmt.execute("create table Employeee123(id integer)");
 			//stmt.execute("INSERT INTO Employeee123 VALUES (123)");
 
-			if(stmt != null)
-			{	
-				stmt.close();
-			}
+			//if(stmt != null)
+			//{	
+			//	stmt.close();
+			//}
 
 			if(conn != null)
 			{
--- JarUpdate/BuildTimePOC.java
+++ JarUpdate/BuildTimePOC.java
@@ -24,9 +24,8 @@ class BuildTimePOC
 		//Util.printList(modifiedFiles);
 		//System.out.println("");
 
+
 		System.out.println(System.currentTimeMillis()+"\n");
-		//File f = new File(projDir+"/sync-readme.md");
-		//System.out.println("LastModified Time : "+f.lastModified());
 
 		long startTime = System.currentTimeMillis();
 
@@ -34,15 +33,14 @@ class BuildTimePOC
 
 		File snapshotFile = new File(projDir+"/"+snapshotPath);
 
-		//filter.setTimeStamp(Long.parseLong("1532710354139"));
-
+		//filter.setTimeStamp(Long.parseLong("1532770544719"));
 		filter.setTimeStamp(snapshotFile.lastModified());
 
 		SearchInDir(new File(projDir), modifiedFiles);
-		printListFiles(modifiedFiles, ".class");
+
+		printListFiles(modifiedFiles, ".java");
 
 		System.out.println("\nTime taken : "+(System.currentTimeMillis()-startTime));
-		
 	}
 
 	void printListFiles(ArrayList<File> list, String ext)
@@ -88,12 +86,21 @@ class LastModifiedFilter implements FileFilter
 
 	public boolean accept(File file)
 	{
-		if(file.isDirectory())
-			return !file.getName().equals("rules") && !file.getName().equals("build");
-
 		String fileName = file.getName();
 
-		return ( (fileName.endsWith(".java") || fileName.endsWith(".class")) && (file.lastModified() > timeStamp));
+		if(file.isDirectory())
+		{
+			return (!fileName.equals("rules") && !fileName.equals("build") && !fileName.startsWith("."));
+		}
+
+		//String absolutePath = file.getAbsolutePath();
+
+		//if(absolutePath.endsWith(".class") && absolutePath.indexOf("com/pega/") == -1)
+		//{
+		//	System.out.println("No com/pega : "+absolutePath);
+		//}
+
+		return ( (fileName.endsWith(".java") || fileName.endsWith(".class")) && file.lastModified() > timeStamp );
 	}
 
 	void setTimeStamp(long timeStamp)
@@ -101,3 +108,80 @@ class LastModifiedFilter implements FileFilter
 		this.timeStamp = timeStamp;
 	}
 }
+
+
+
+/*
+Java Files found false, Class files found : false  Dir Name : /home/manoj/all/work/master/prpc-platform/archives
+Java Files found false, Class files found : false  Dir Name : /home/manoj/all/work/master/prpc-platform/db
+Java Files found false, Class files found : false  Dir Name : /home/manoj/all/work/master/prpc-platform/dist
+Java Files found false, Class files found : false  Dir Name : /home/manoj/all/work/master/prpc-platform/gradle
+Java Files found false, Class files found : false  Dir Name : /home/manoj/all/work/master/prpc-platform/pipeline
+
+
+Java Files found true, Class files found : false  Dir Name : /home/manoj/all/work/master/prpc-platform/addons
+Java Files found false, Class files found : true  Dir Name : /home/manoj/all/work/master/prpc-platform/buildSrc
+Java Files found true, Class files found : true  Dir Name : /home/manoj/all/work/master/prpc-platform/core
+Java Files found true, Class files found : false  Dir Name : /home/manoj/all/work/master/prpc-platform/doc
+Java Files found true, Class files found : true  Dir Name : /home/manoj/all/work/master/prpc-platform/dsm
+Java Files found true, Class files found : true  Dir Name : /home/manoj/all/work/master/prpc-platform/etc
+Java Files found false, Class files found : true  Dir Name : /home/manoj/all/work/master/prpc-platform/infrastructure
+Java Files found true, Class files found : true  Dir Name : /home/manoj/all/work/master/prpc-platform/platform
+Java Files found true, Class files found : true  Dir Name : /home/manoj/all/work/master/prpc-platform/test
+Java Files found true, Class files found : true  Dir Name : /home/manoj/all/work/master/prpc-platform/tools
+
+
+
+		SearchInDir(new File("/home/manoj/all/work/master/prpc-platform/addons"), modifiedFiles);
+		SearchInDir(new File("/home/manoj/all/work/master/prpc-platform/buildSrc"), modifiedFiles);
+		SearchInDir(new File("/home/manoj/all/work/master/prpc-platform/core"), modifiedFiles);
+		SearchInDir(new File("/home/manoj/all/work/master/prpc-platform/doc"), modifiedFiles);
+		SearchInDir(new File("/home/manoj/all/work/master/prpc-platform/dsm"), modifiedFiles);
+		SearchInDir(new File("/home/manoj/all/work/master/prpc-platform/etc"), modifiedFiles);
+		SearchInDir(new File("/home/manoj/all/work/master/prpc-platform/infrastructure"), modifiedFiles);
+		SearchInDir(new File("/home/manoj/all/work/master/prpc-platform/platform"), modifiedFiles);
+		SearchInDir(new File("/home/manoj/all/work/master/prpc-platform/test"), modifiedFiles);
+		SearchInDir(new File("/home/manoj/all/work/master/prpc-platform/tools"), modifiedFiles);
+
+
+		
+		ArrayList<String> listOfDir = new ArrayList<String>();
+
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/addons");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/archives");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/buildSrc");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/core");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/db");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/dist");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/doc");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/dsm");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/etc");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/gradle");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/infrastructure");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/pipeline");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/platform");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/test");
+		listOfDir.add("/home/manoj/all/work/master/prpc-platform/tools");
+
+		for(int i=0; i<listOfDir.size(); i++)
+		{
+			ArrayList<File> filesList = new ArrayList<File>();
+
+			SearchInDir(new File(listOfDir.get(i)), filesList);
+
+			boolean foundJavaFile = false;
+			boolean foundClassFile = false;
+
+			for(int j=0; j<filesList.size(); j++)
+			{
+				if(filesList.get(j).getName().endsWith(".java"))
+					foundJavaFile = true;
+
+				if(filesList.get(j).getName().endsWith(".class"))
+					foundClassFile = true;
+			}
+
+			System.out.println("Java Files found "+foundJavaFile+", Class files found : "+foundClassFile+"  Dir Name : "+listOfDir.get(i));
+		}
+		
+*/
