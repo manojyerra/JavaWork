048a4ebd66e24d2a9bda8c01984ffb5298cb11b0
manojyerra <manojyerra@gmail.com>
2019-03-11 01:10:30 +0530

    Modified JavaTeluguDemo class

--- ReadingDataFromOL/Telugu-English/support/JavaTeluguDemo.java
+++ ReadingDataFromOL/Telugu-English/support/JavaTeluguDemo.java
@@ -7,6 +7,9 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+import java.awt.*;
+
+
 /**
 * Copy gautami.ttf from windows\fonts to \java_160_10\jre\lib\fonts\
 *
@@ -30,17 +33,38 @@ import java.util.*;
 * type in telugu.
 */
 
-public class JavaTeluguDemo extends JFrame
+public class JavaTeluguDemo
 { 
-	private static final long serialVersionUID = 123123123L;
-	private static final Font fontTeluguGautami32 = new Font("gautami",	Font.PLAIN, 32);
-	private static final Font fontTeluguGautami10 = new Font("gautami",	Font.PLAIN, 22);
+	private String telugu = "\u0C05\u0C2E\u0C4D\u0C2E";
+	private String teluguHeading = "\u0C1C\u0C3E\u0C35\u0C3E \u0C24\u0C46\u0C32\u0C41\u0C17\u0C41";
 	
-	private static String telugu = "\u0C05\u0C2E\u0C4D\u0C2E";
-	private static String teluguHeading = "\u0C1C\u0C3E\u0C35\u0C3E \u0C24\u0C46\u0C32\u0C41\u0C17\u0C41";
+	public static void main(String[] args)  throws Exception
+	{
+		new JavaTeluguDemo();
+	}
  
 	public JavaTeluguDemo() throws Exception
 	{
+		createFont("fonts/Pothana.ttf");
+		createFont("fonts/Lohit.ttf");
+		
+		//Lohit
+		//Vemana2000
+		//Pothana2000
+		
+		Font fontLohit = new Font("Lohit Telugu", Font.PLAIN, 32);
+		Font fontPothana = new Font("Pothana2000", Font.PLAIN, 22);
+
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		 
+		JFrame frame1 = new JFrame();
+		frame1.setSize(400, 150);
+		frame1.setVisible(true);
+		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 
+		frame1.getLayeredPane().getComponent(1).setFont(fontLohit);
+		frame1.setTitle(telugu);	 
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("SaveHere.rtf"), "UTF-8"));				
 
 		String totText = "";
@@ -50,21 +74,16 @@ public class JavaTeluguDemo extends JFrame
 			totText += line;
 
 		JTextArea taDisplay = new JTextArea(totText);
-		taDisplay.setFont(fontTeluguGautami32);
-		this.getContentPane().add(taDisplay);
+		taDisplay.setFont(fontPothana);
+		frame1.getContentPane().add(taDisplay);
 	}
-	 
-	public static void main(String[] args)  throws Exception
+	
+	public void createFont(String fontFilePath)
 	{
-		// We can't change title font in default system look and feel.
-		JFrame.setDefaultLookAndFeelDecorated(true);
-		 
-		JFrame frame1 = new JavaTeluguDemo();
-		frame1.setSize(400, 150);
-		frame1.setVisible(true);
-		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 
-		frame1.getLayeredPane().getComponent(1).setFont(fontTeluguGautami10);
-		frame1.setTitle(teluguHeading);	 
+		try {
+			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(fontFilePath)));
+		} catch (IOException|FontFormatException e) {			
+		}		
 	}
 }
